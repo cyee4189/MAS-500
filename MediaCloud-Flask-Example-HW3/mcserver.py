e3accbabdbe59e8788635e200382548a7b9cba66
cyee4189 <cyee4189@gmail.com>
2017-12-07 15:37:03 -0500

    Add files via upload

--- MediaCloud-Flask-Example-HW3/mcserver.py
+++ MediaCloud-Flask-Example-HW3/mcserver.py
@@ -18,8 +18,25 @@ config = ConfigParser.ConfigParser()
 config.read(os.path.join(basedir, 'settings.config'))
 
 # set up logging
-logging.basicConfig(level=logging.DEBUG)
-logging.info("Starting the MediaCloud example Flask app!")
+# logging.basicConfig(level=logging.DEBUG)
+# logging.info("Starting the MediaCloud example Flask app!")
+
+if not app.debug and os.environ.get('HEROKU') is None:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('mediacloud app')
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('mediacloud app')
 
 # clean a mediacloud api client
 mc = mediacloud.api.MediaCloud( config.get('mediacloud','api_key') )
