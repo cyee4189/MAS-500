88d54002bedab5ca2c0f4eed55e58b4157854e5e
cyee4189 <cyee4189@gmail.com>
2017-12-06 21:26:47 -0500

    Delete test_electioncount.py

--- test_electioncount.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import electioncount
-
-class ElectionCountTest(unittest.TestCase):
-
-    def test_TrumpCount(self):
-        result = electioncount.ElectionCount('Trump')
-        self.assertEqual(result, 33660)
